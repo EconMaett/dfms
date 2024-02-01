650e670dfef3926a9aa11b917b7c8180cd7ebe55
EconMaett <matthias.spichiger@bluewin.ch>
2024-02-01 15:19:01 +0100

    Initial commit

---
+++ R/01_introduction.R
@@ -0,0 +1,15 @@
+# Introducing dfms: Efficient Estimation of Dynamic Factor Models in R -----
+
+# The dfms R package is available on:
+# - CRAN: 
+# - GitHub: https://github.com/SebKrantz/dfms
+# - Website: https://sebkrantz.github.io/dfms/
+
+# R-bloggers: https://www.r-bloggers.com/2022/10/introducing-dfms-efficient-estimation-of-dynamic-factor-models-in-r/
+
+
+## Setup ----
+
+# The R package is available on CRAN
+# install.packages("dfms")
+library(dfms)
